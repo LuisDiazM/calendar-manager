4db8cb94b9c5a8a67780c5f56fc23a796c4f4055
LuisDiazM <luismigueldiazmorales@gmail.com>
2023-05-27 13:51:59 -0500

    chore: build docker application

--- backend/calendarNotificationEvents/cmd/config/environment.go
+++ backend/calendarNotificationEvents/cmd/config/environment.go
@@ -19,12 +19,10 @@ type Env struct {
 func NewEnvironmentsSpecification() *Env {
 	var env Env
 	viper.SetConfigFile("cmd/config/.env")
-
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Fatal("Can't find the file .env : ", err)
 	}
-
 	err = viper.Unmarshal(&env)
 	if err != nil {
 		log.Fatal("Environment can't be loaded: ", err)
--- frontend/calendar-manager-ui/src/app/environments/environment.prod.ts
+++ frontend/calendar-manager-ui/src/app/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+
 export const environments = {
     GOOGLE_CLIENT: '74994666463-ilp90899u78hq4dnuknaenct40fn8pa9.apps.googleusercontent.com',
     CALENDAR_MANAGER_API: "http://localhost:9090"
